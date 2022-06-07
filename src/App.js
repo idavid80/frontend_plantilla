bb32a04e3afba496d803e1ae3ceb26ab06bb0f45
idavid80 <dominguezbueno.david@gmail.com>
2022-06-07 20:20:27 +0200

    actualizacion frontend

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-app.use(express.static(path.join(__dirname, 'build')));
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
--app.get('/', function (req, res) {
-+app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- })
-});
+  callAPI() {
+    fetch(SERVER_URL)
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
+  }
 
+  componentDidMount() {
+    this.callAPI();
+    console.log(SERVER_URL)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
